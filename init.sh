feef10faef4bc55675fc92ccdbf949d21dcdcd5b
Lils2013 <aleksandr.tsoy@phystech.edu>
2017-01-22 23:10:41 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo etc/init.d/nginx restart
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
